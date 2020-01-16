8dd38ccd27dfeeb91f7027baa2678fb195210baf
Theodore McNulty <theodoremcnulty@Theodores-Air.fios-router.home>
2020-01-15 19:30:02 -0500

    added build

---
+++ client/build/precache-manifest.4e23ae6a547a7cf765800faa65762cb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3de741ff702120997bdc49e9fbeeeede",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7429c3f781ac43c686a",
+    "url": "/static/css/main.141f75e9.chunk.css"
+  },
+  {
+    "revision": "e058c64248156852eb3a",
+    "url": "/static/js/2.383da596.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.383da596.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7429c3f781ac43c686a",
+    "url": "/static/js/main.1b21bfb8.chunk.js"
+  },
+  {
+    "revision": "5274233e5f14eabd9ee9",
+    "url": "/static/js/runtime-main.38a53adb.js"
+  },
+  {
+    "revision": "d0d7f78423891fc622d41aa0d82630b5",
+    "url": "/static/media/Spotify_Icon_CMYK_White.d0d7f784.png"
+  }
+]);
